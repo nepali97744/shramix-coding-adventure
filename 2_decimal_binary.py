5ecd299b18fd8db0da3e72fb2fccc5a3d953cbe8
Shramik Thakali <shramik182@gmail.com>
2025-10-04 15:38:51 +0100

    chnaing binary formula

--- 2_decimal_binary.py
+++ 2_decimal_binary.py
@@ -7,7 +7,7 @@ def main():
     user_decimal = int(input("Please enter a number to convert to binary: "))
     final_binary = ""
     while user_decimal / 2 !=0:
-        remainder = (user_decimal - user_decimal%2)//2
+        remainder = (user_decimal)//2
         temp_binary = user_decimal%2
         final_binary = str(temp_binary) + final_binary
         user_decimal = remainder
