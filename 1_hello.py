73575c5bb3c2060e81d70f09d934b1ad14c3c4d4
Shramik Thakali <shramik182@gmail.com>
2025-10-04 12:36:06 +0100

    added default name

--- 1_hello.py
+++ 1_hello.py
@@ -1,7 +1,14 @@
 # 1.	Hello, World!
 
 def main():
-    print ("Hello, World!")
+    
+    print (f"Hello, {name()}")
 
+def name(default = "world"):
+    name = input("What is your name? ")
+    if name == "":
+        return default
+    else:
+        return (name)
 if __name__ == "__main__":
     main()
