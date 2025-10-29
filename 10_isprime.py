9de4b9d05434dba463f993a82443574119dc8a14
Shramik Thakali <shramik182@gmail.com>
2025-10-29 13:32:33 +0000

    Added try again feature

--- 10_isprime.py
+++ 10_isprime.py
@@ -1,14 +1,28 @@
 # 10.	Check if a number is prime
 
 def main():
-    user_input = int(input("Enter a number to check if it's a prime number: "))
+    
 
+    while True:
+        try:
+            user_input = int(input("Enter a number to check if it's a prime number: "))
+            print(is_prime(user_input))
+
+            try_again = input("Do you want to continue? (Y/N): ").lower()
+            if try_again == "y":
+                continue
+            else:
+                break
+        except:
+            print("Please only enter a valid number. ")
+
+def is_prime(user_input):
     for i in range(2, user_input-1):
         if user_input % (i) == 0:
-            break
+            return (f"{user_input} is not a prime number.")
+            
     else:
-        print ("is prime")
-        
+        return (f"{user_input} is a prime number.")
         
     
 
