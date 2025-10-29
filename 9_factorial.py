f0e833c51441b7a2f79f46684cad5286d7e91a4e
Shramik Thakali <shramik182@gmail.com>
2025-10-29 13:17:41 +0000

    Added try again feature

--- 9_factorial.py
+++ 9_factorial.py
@@ -7,13 +7,29 @@ def main():
         try:
             user_input = int(input("Enter a number to find factorial of: "))
             total = 1 
-            for i in range(user_input):
-                total = (i+1) * total
-               
-            print (total)
-            break
+            
+            factorial_finder(user_input, total)
+
+            try_again = input("Do you want to continue? (Y/N): ").lower()
+
+            if try_again == "y":
+                continue
+            else:
+                break
+
+
+
         except:
             print ("Please only enter valid number. Please try again.")
+
+def factorial_finder(user_input, total):
+    for i in range(user_input):
+        total = (i+1) * total
+               
+    print (total)
+            
+
+
 
 if __name__ == "__main__":
     main()
