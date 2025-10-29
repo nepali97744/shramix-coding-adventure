c245a9539c6a9c2d0596b348645ee42a1d5a0f50
Shramik Thakali <shramik182@gmail.com>
2025-10-29 14:26:56 +0000

    added try again plus made the code more efficient

--- 12_greatest_divisor.py
+++ 12_greatest_divisor.py
@@ -1,17 +1,34 @@
 #12.	Find the greatest common divisor (GCD) of two numbers
 
 def main():
-    num_first = int(input("Please enter first number: "))
-    num_second = int(input("Please enter second number: "))
+    while True:
+        try:
+            num_first = int(input("Please enter first number: "))
+            num_second = int(input("Please enter second number: "))
 
-    divisor = num_first + num_second
+            divisor(num_first, num_second)
+            try_again = input("Do you want to continue? (Y/N) ").lower()
+            if try_again == "y":
+                continue
+            else:
+                break
+
+        except:
+            print("Please only enter positive number. ")
+
+
+   
+
+def divisor(num_first, num_second):
+    divisor = min(num_first,  num_second)
     gcd = 1
-    while divisor > gcd:
+    while divisor >= gcd:
         if num_first%divisor == 0 and num_second%divisor == 0:
             print (f"{divisor} is the greatest common divisor.")
             break
         else:
             divisor = divisor-1
+
 
 if __name__ == "__main__":
     main()
