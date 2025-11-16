d1c392d4060ebe2319c42cd14bab9e07ba217f9c
Shramik Thakali <shramik182@gmail.com>
2025-11-16 13:43:32 +0000

    just updating

--- 11_fibonacci.py
+++ 11_fibonacci.py
@@ -1,7 +1,12 @@
 #11.	Print the Fibonacci sequence
 
 def main():
-    pass
+    user_input = int("Please enter a number to create the fibonacci sequence")
+
+    if user_input == 0:
+        print ("0")
+    elif user_input == 1:
+        print()
 
 if __name__ == "__main__":
     main()
