264a29733ba7dc981824c944c7a5f0cd634a269a
Shramik Thakali <shramik182@gmail.com>
2025-11-16 14:20:39 +0000

    added replayability

--- 14_reverse_string.py
+++ 14_reverse_string.py
@@ -1,15 +1,42 @@
 #14.	Reverse a string
 
 def main():
-    user_input = input("Please enter a word you want reversed: ")
-    reversed_word = ""
+    
+    
+    while True:
+        try:
+            user_input = input("Please enter a word you want reversed: ")
+            reversed(user_input)
+            continue_again = input("Do you want to continue? (Y/N): ").lower()
+            
+            while True:
 
-    for _ in user_input:
-        reversed_word = _ + reversed_word
+                if continue_again == "y":
+                    break
+                elif continue_again == "n":
+                    return ("Thank you for playing.")
+                else:
+                    continue_again = input("Please type either y to continue or n to exit: ").lower()
+        except:
+            print("Please enter a valid word.")
 
-    print (reversed_word)
+
+    
 
 
+
+def reversed(user_input):
+    
+    while user_input == "":
+        print("Please enter a word.")
+        break
+    else:
+
+        reversed_word = ""
+        for _ in user_input:
+            reversed_word = _ + reversed_word
+        
+        print (reversed_word)
 
 if __name__ == "__main__":
     main()
