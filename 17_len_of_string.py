0ea8f462e3d3118d0da1a23a8f210c7ecd30c876
Shramik Thakali <shramik182@gmail.com>
2025-11-17 10:50:23 +0000

    added try again feature

--- 17_len_of_string.py
+++ 17_len_of_string.py
@@ -1,9 +1,20 @@
 #17.	Find the length of a string without using the len() function
 
 def main():
-    user_input = input("Plase enter a word to count: ")
     
-    len_string(user_input)
+    
+    while True:
+        try:
+            user_input = input("Plase enter a word to count: ")
+            len_string(user_input)
+            
+            if try_again():
+                pass
+            else:
+                break
+            
+        except:
+            print("Please enter a valid input.")
     
     
 
@@ -14,6 +25,16 @@ def len_string(user_input):
 
     print (total)
 
+def try_again():
+    while True:
+        user_input = input("Do you want to continue? (Y/N): ").lower()
+        if user_input == "y":
+            return True
+        elif user_input == "n":
+            print ("Thank you for playing.")
+            return False
+        else:
+            print("Please type Y to continue or N to exit.")
 
 if __name__ == "__main__":
     main()
