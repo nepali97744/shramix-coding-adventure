a2c14c3f0474f180689f98d0421e5ad9d58ccbc9
Shramik Thakali <shramik182@gmail.com>
2026-02-19 11:06:50 +0000

    multi page article scrapper

--- 30_scrapper.py
+++ 30_scrapper.py
@@ -1,5 +1,5 @@
 import json
-from turtle import title
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,19 +9,35 @@ def main():
     raw_text = website.text
     raw_soup = BeautifulSoup(raw_text, "html.parser")
 
-    titles = raw_soup.find_all("h3", class_ = "alith_post_title")[:10]
+    titles = raw_soup.find_all("h3", class_ = "alith_post_title")[:5]
     
     for title in titles:
         link = title.find("a")
         if link and title.text.strip():
 
             article_link = link.get("href")
-            article_title = title.text
+            
+            article_title = title.text.strip()
+            article_title_soup = requests.get(article_link, "html.parser")
+            article_soup = BeautifulSoup(article_title_soup.text, "html.parser")
 
-            print (article_title.strip())
+            print (article_title)
             print(article_link)
-            print()
-            print()
+            
+            articles = article_soup.find_all("div", class_ = "dropcap column-1 animate-box")
+            
+            
+            for article in articles:
+                article_text_all = article.find_all("p")
+                if article_text_all:
+                    for article1 in article_text_all:
+                        print (article1.text)
+                    
+
+                
+                
+                print()
+                print()
       
 
     
