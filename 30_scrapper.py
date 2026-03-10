34fde35c46f9a137e4fc389635778e622d21d480
Shramik Thakali <shramik182@gmail.com>
2026-03-10 18:13:24 +0000

    added session to everything and try except

--- 30_scrapper.py
+++ 30_scrapper.py
@@ -21,15 +21,18 @@ def main():
 
         #titles = raw_soup.find_all("h3", class_ = "alith_post_title")[:2]
 
-        titles = website_to_scrape("https://thehimalayantimes.com/")
+        titles = website_to_scrape(session, "https://thehimalayantimes.com/")
         write_to_file(session, time_string, titles)
     
 
-def website_to_scrape(website):
-    raw_data = requests.get(website)
-    website_soup = BeautifulSoup(raw_data.text, "html.parser")
-    titles = website_soup.find_all("h3", class_ = "alith_post_title" )[:20]
-    return (titles)
+def website_to_scrape(session, website):
+    try:
+        raw_data = session.get(website)
+        website_soup = BeautifulSoup(raw_data.text, "html.parser")
+        titles = website_soup.find_all("h3", class_ = "alith_post_title" )
+        return (titles)
+    except:
+        return []
 
     
 
@@ -47,28 +50,32 @@ def write_to_file(session, time_string,titles):
                     article_link = link.get("href")
                     
                     article_title = title.text.strip()
-                    article_title_soup = session.get(article_link)
-                    article_soup = BeautifulSoup(article_title_soup.text, "html.parser")
+                    try:
+                        article_title_soup = session.get(article_link)
+                        article_soup = BeautifulSoup(article_title_soup.text, "html.parser")
 
-                    
-                    
-                    f.write(article_title + "\n")
-                    f.write(article_link + "\n")
+                        
+                        
+                        f.write(article_title + "\n")
+                        f.write(article_link + "\n")
 
-                    # print (article_title)
-                    # print(article_link)
-                    
-                    articles = article_soup.find_all("div", class_ = "dropcap column-1 animate-box")
+                        # print (article_title)
+                        # print(article_link)
                         
-                        
-                    for article in articles:
-                        
-                        article_text_all = article.find_all("p")
-                        if article_text_all:
-                            for article1 in article_text_all:
-                                f.write(article1.text + "\n")
-                             
-                            f.write("-" * 100 + "\n\n\n\n")
+                        articles = article_soup.find_all("div", class_ = "dropcap column-1 animate-box")
+                            
+                            
+                        for article in articles:
+                            
+                            article_text_all = article.find_all("p")
+                            if article_text_all:
+                                for article1 in article_text_all:
+                                    f.write(article1.text + "\n")
+                                
+                                f.write("-" * 100 + "\n\n\n\n")
+                    except:
+                        print ("No article found.")
+                        continue
                      
                 print(f"Articles: {count}")
                     #time.sleep(1)
